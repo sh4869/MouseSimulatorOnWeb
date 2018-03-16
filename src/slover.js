6a1a925e4e039a8955d50e78d6038fcbc19b85ab
sh4869 <nobuk4869@gmail.com>
2018-03-17 07:59:12 +0900

    Fix adachi slover

--- src/slover.js
+++ src/slover.js
@@ -27,32 +27,37 @@ Queue.prototype.toString = function () {
 const slove = (mapsize, maze, goals) => {
     // wmap[x][y]
     // pos = [x,y]
-    let wmap = Array.from({ length: mapsize[0] }, (i, v) => Array.from({ length: mapsize[1] }, (i, v) => 255))
+    let wmap = Array.from({ length: mapsize[1] }, (i, v) => Array.from({ length: mapsize[0] }, (i, v) => 255))
     var q = new Queue();
     for (let i = 0; i < goals.length; i++) {
         q.enqueue(goals[i])
-        wmap[goals[i][0]][goals[i][1]] = 0
+        wmap[goals[i][1]][goals[i][0]] = 0
     }
+    console.log(maze)
     let cpos = q.dequeue();
     while (cpos !== null) {
-        console.log(q.toString())
-        let count = wmap[cpos[0]][cpos[1]];
-        const number = cpos[0] + cpos[1]  * mapsize[1];
-        if (maze[number][0] == 0 && cpos[0]+1 < mapsize[0] && wmap[cpos[0] + 1][cpos[1]] == 255) {
-            wmap[cpos[0] + 1][cpos[1]] = count + 1;
-            q.enqueue([cpos[0] + 1, cpos[1]])
-        }
-        if (maze[number][1] == 0 && cpos[1]+1 < mapsize[1] && wmap[cpos[0]][cpos[1] + 1] == 255) {
-            wmap[cpos[0]][cpos[1] + 1] = count + 1;
+        let count = wmap[cpos[1]][cpos[0]];
+        const number = cpos[0] + cpos[1] * mapsize[1];
+        // console.log(number,cpos)
+        // Top
+        if (maze[number][0] == 0 && cpos[1] + 1 < mapsize[1] && wmap[cpos[1] + 1][cpos[0]] === 255) {
+            wmap[cpos[1] + 1][cpos[0]] = count + 1;
             q.enqueue([cpos[0], cpos[1] + 1])
         }
-        if (maze[number][2] == 0 && cpos[0]-1 > -1 && wmap[cpos[0] - 1][cpos[1]] == 255) {
-            wmap[cpos[0] - 1][cpos[1]] = count + 1;
-            q.enqueue([cpos[0] - 1, cpos[1]])
+        // Right
+        if (maze[number][1] == 0 && cpos[0] + 1 < mapsize[0] && wmap[cpos[1]][cpos[0] + 1] === 255) {
+            wmap[cpos[1]][cpos[0] + 1] = count + 1;
+            q.enqueue([cpos[0] + 1, cpos[1]])
         }
-        if (maze[number][3] == 0 && cpos[1]-1 > -1 && wmap[cpos[0]][cpos[1] - 1] == 255) {
-            wmap[cpos[0]][cpos[1] - 1] = count + 1;
+        // Bottom
+        if (maze[number][2] == 0 && cpos[1] - 1 > -1 && wmap[cpos[1] - 1][cpos[0]] === 255) {
+            wmap[cpos[1] - 1][cpos[0]] = count + 1;
             q.enqueue([cpos[0], cpos[1] - 1])
+        }
+        // Left
+        if (maze[number][3] == 0 && cpos[0] - 1 > -1 && wmap[cpos[1]][cpos[0] - 1] === 255) {
+            wmap[cpos[1]][cpos[0] - 1] = count + 1;
+            q.enqueue([cpos[0] - 1, cpos[1]])
         }
         cpos = q.dequeue();
     }
