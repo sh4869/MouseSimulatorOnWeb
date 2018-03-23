cedb334b0d1cb62728383e4632b623730a0cea57
sh4869 <nobuk4869@gmail.com>
2018-03-23 22:45:28 +0900

    Add Travis files

---
+++ scripts/deploy.js
@@ -0,0 +1,25 @@
+const ghpages = require("gh-pages");
+const path = require("path");
+
+const GH_TOKEN = process.env.GH_TOKEN;
+const repo = "@github.com:sh4869/MouseSimulatorOnWeb.git"
+ghpages.publish(
+    path.join(__dirname, '..', 'build'),
+    {
+        repo: 'https://' + GH_TOKEN + repo,
+        user: {
+            name: "sh4869",
+            email: "nobuk4869@gmail.com",
+        },
+        message: "[ci skip] UPDATEE WEBPAGE",
+        branch: "gh-pages"
+    },
+    function (err) {
+        if(err){
+            console.log(err);
+            process.exit(1);
+        } else {
+            console.log("Update WebPage");
+        }
+    }
+);
